2d37e6afa95e5a46879e7391eb9950d20a95a586
Nitro <nitro0@naver.com>
2020-03-31 19:24:07 +0900

    New Version updated.
    https://www.youtube.com/watch?v=RDQK1_SWFuc

---
+++ Random Map Generator/Assets/Editor/MapEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapGenerator))]
+public class MapEditor : Editor {
+    public override void OnInspectorGUI() {
+        MapGenerator mapGenerator = (MapGenerator)target;
+
+        if (DrawDefaultInspector())
+            if (mapGenerator.autoUpdate)
+                mapGenerator.GenerateMap();
+
+        if (GUILayout.Button("Generate Map"))
+            mapGenerator.GenerateMap();
+    }
+}
---
+++ Random Map Generator/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+    public enum DrawMode { NoiseMode, ColorMode};
+    public DrawMode drawMode;
+
+    public int mapWidth = 50;
+    public int mapHeight = 50;
+    public float noiseScale;
+
+    public int octaves;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public bool autoUpdate;
+
+    public TerrainType[] regions;
+
+    public void GenerateMap() {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        Color[] colorMap = new Color[mapWidth * mapHeight];
+        for (int y = 0; y < mapHeight; y++) {
+            for (int x = 0; x < mapWidth; x++) {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++) {
+                    if (currentHeight <= regions[i].height) {
+                        colorMap[y * mapWidth + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+
+        if (drawMode == DrawMode.NoiseMode)
+            display.DrawTexture(TextureGenerator.TextureFromNoiseMap(noiseMap));
+        else if (drawMode == DrawMode.ColorMode)
+            display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+    }
+
+    void OnValidate() {
+        if (mapWidth < 1) mapWidth = 1;
+        if (mapHeight < 1) mapHeight = 1;
+        if (lacunarity < 1) lacunarity = 1;
+        if (octaves < 1) octaves = 0;
+    }
+
+    [System.Serializable]
+    public struct TerrainType {
+        public string name;
+        public float height;
+        public Color color;
+    }
+}
